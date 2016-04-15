098b8238771df873ab7593bfe492e9a831364d33
Wei Xian Low <weix.low@gmail.com>
2016-04-14 22:31:14 -0500

    updated UI for other scene

--- src/wlkr7employeerecord/EditEmployeeController.java
+++ src/wlkr7employeerecord/EditEmployeeController.java
@@ -14,7 +14,7 @@ import javafx.fxml.Initializable;
  *
  * @author weixianlow
  */
-public class SearchEmployeeController implements Initializable {
+public class EditEmployeeController implements Initializable {
 
     /**
      * Initializes the controller class.
